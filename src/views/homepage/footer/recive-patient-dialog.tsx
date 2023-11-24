d0b4a09f8da7b48b919aa9bce9d2087affcaf760
oron.mozes <oron.mozes@khealth.com>
2023-11-24 15:22:32 +0200

    receive modal

---
+++ src/views/homepage/footer/recive-patient-dialog.tsx
@@ -0,0 +1,41 @@
+import { Button, Dialog, Portal, Text } from "react-native-paper";
+import { useTranslation } from "../../../hooks/useMyTranslation";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigation } from "../../../interfaces";
+import { ROUTES } from "../../../routes";
+
+export function ReceivePatientDialog({ onClose }: { onClose(): void }) {
+  const translation = useTranslation();
+  const navigation = useNavigation<StackNavigation>();
+
+  return (
+    <Portal>
+      <Dialog visible={true} onDismiss={onClose}>
+        <Dialog.Title testID="receive-dialog-title">
+          {translation("receiveMethod")}
+        </Dialog.Title>
+
+        <Dialog.Actions style={{ justifyContent: "flex-start" }}>
+          <Button
+            onPress={() => {
+              onClose();
+            }}
+            mode="contained"
+            testID="receive-dialog-nfc-method"
+          >
+            {translation("selectNFCMethod")}
+          </Button>
+          <Button
+            onPress={() => {
+              navigation.navigate(ROUTES.IMPORT_PATIENT);
+              onClose();
+            }}
+            testID="receive-dialog-qr-method"
+          >
+            {translation("selectQrMethod")}
+          </Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>
+  );
+}
